0e82f0101fea86dbff9a08d1add937e991238a57
Your Name <you@example.com>
2019-09-17 05:52:08 +0200

    first commit2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./components/Form";
+import "./App.css";
 
 class App extends Component {
+  getRecipe = event => {
+    event.preventDefault();
+    //const recipeName = event.target.elements.recipeName.value;
+    const recipeName = event.target.value;
+    console.log("dela" + recipeName);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
